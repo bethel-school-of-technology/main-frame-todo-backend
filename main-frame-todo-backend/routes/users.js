4332ae9a5085fdc13a7559b638da21951118b8c4
James Bean <jamesbean@Jamess-MacBook-Pro.local>
2020-10-16 20:34:37 -0400

    "second commit"

--- main-frame-todo-backend/routes/users.js
+++ main-frame-todo-backend/routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+for (users(let , index = 0); index < array.length; index++)
+  {
+  const element = array[index];
+  
+}
+{
+  create,
+  list,
+  read,
+  remove,
+  update
+  = require("../controllers/)");
+}
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
 module.exports = router;
