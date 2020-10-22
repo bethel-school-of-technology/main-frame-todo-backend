d203b497d09f8c65b69d191918367ee37c4faf58
James Bean <jamesbean@Jamess-MacBook-Pro.local>
2020-10-22 16:08:23 -0400

    fixing repo

--- main-frame-todo-backend/db/mongoose.js
+++ main-frame-todo-backend/db/mongoose.js
@@ -1,7 +1,16 @@
 // this file will connect to the db
 const mongoose = require('mongoose');
 mongoose.promise=global.promise;
-mongoose.connect;
+mongoose.connect('mongodb+srv://dbuser:Password1!@cluster0.g6zcn.mongodb.net/todo?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+    console.log("Connected to MongoDB successfully");
+}).catch((e)=>{
+    console.log("Error while attempting to connect to MongoDB");
+    console.log(e);
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
 
 module.exports = {
     mongoose
